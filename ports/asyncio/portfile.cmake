f60d0ecfc473065bb42f3c8fd8d4b889090ee44c
Hackerl <490021209@qq.com>
2025-08-29 16:03:15 +0800

    upgrade asyncio to 1.0.5-4

--- ports/asyncio/portfile.cmake
+++ ports/asyncio/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Hackerl/asyncio
-        REF d01eb1c9e970a59a663a4345168ea0d33c8e9bb2
-        SHA512 cd5cc51abb50cf6ccd73c843b0f4b0fb708cf49e1086adf459fd31899743825adf3ce6243c1f2c0673f28f8f2d4e028b59d41ab79e95d2d5d466b7a33e3c29db
+        REF d4aad5030509fcda8c857ea84fb3ead6eb84be76
+        SHA512 3479f6c6ea04711dc5192df1e48c54d97d42b47adfe14ab3b9c36dcc46f87e3e9337333152037a4dee9ee6fca81405c70010e2ae44d75ec899528b1f04867bf8
 )
 
 vcpkg_configure_cmake(
