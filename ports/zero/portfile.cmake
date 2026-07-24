218ddd8d1f2ef4e52b63e248a9d7cf3c59212e5a
Hackerl <490021209@qq.com>
2026-07-24 16:39:46 +0800

    upgrade zero to 1.2.0-5

--- ports/zero/portfile.cmake
+++ ports/zero/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Hackerl/zero
-        REF 0e5b3509962f72847b988b1463dc07fef09b42a7
-        SHA512 3452f40db3340fcbe1508fdae57219311b7c28d83db6e5e2879294b596ccdbba72ad3301cc6d9539adf91772bc453f97cd9b09c04fdce5de51ce12d540c93db8
+        REF c9b106aaeec9894c6b30b4ce1216e13a5ba37418
+        SHA512 c1491bc82b771272a6b8a317631979140eb34d3195e0f0715fabee2d2d3f3a5abd3462503b6098dab80a9befb4942bc8966b04a0dbbea9d8276314c531e6498c
 )
 
 vcpkg_configure_cmake(
